a785d0738cf7e15e049d9c0941b33b6249138c41
milwest <milwestnet@gmail.com>
2025-07-02 14:27:11 +0300

    Нова версія

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  experimental: {
+    appDir: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true 
+  },
 };
 
 module.exports = nextConfig;
